8d21439efbe89194640351968f4814291ecdcfb7
Aykan Ugur <aykanugur157@gmail.com>
2025-03-27 14:53:24 +0300

    bug

--- Lab 6/main.cpp
+++ Lab 6/main.cpp
@@ -12,25 +12,20 @@ using namespace std;
 
 
 double x = 0;
-void question3(double n) {
+double question3(double n) {
     if (n >0)
     {
         x += 1/n;
         cout << "1/" << n << endl;
         question3(n-1);
     }else {
-        cout << x << endl;
+        return x;
     }
 }
-void question3() {
+double question3() {
     cout << "enter val" << endl;
     double n;
     cin >> n;
-    question3(n);
+    return question3(n);
 }
 
-int main() {
-    question3(3);
-    question3();
-    return 0;
-}
